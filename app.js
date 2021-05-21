406ee151a6b1a198755ea5fd0ca9d475c20c526c
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-05-21 20:17:25 -0300

    create order and close orders done

--- app.js
+++ app.js
@@ -33,21 +33,30 @@ function convertString(string) {
     return string;
 }
 
-function marketOrder(pair) {
+/**
+ * Create the market order
+ * @param {string} pair eth or btc
+ * @param {string} side buy or sell
+ * @param {string} size size of the order
+ */
+function marketOrder(pair, side, size) {
     ftx.request({
         method: 'POST',
         path: '/orders',
         data: {
             market: convertString(pair),
-            size: 0.001,
-            side: 'buy',
+            size: size,
+            side: side,
             type: 'market',
             price: null
         }
-    }).then(console.log).catch(console.log);
+    }).then(console.log).catch(err => console.log(err));
 }
 
-async function closeOrders(pair) {
+/**
+ * This function will first get all positions, filter through them and close them all
+ */
+async function closeOrders() {
     let request = ftx.request({
         method: 'GET',
         path: '/positions'
@@ -55,23 +64,29 @@ async function closeOrders(pair) {
 
     let result = await request;
     let onlyPositionsWithSize = result.result.filter(pos => pos.size !== 0);
-    console.log(onlyPositionsWithSize)
 
-    // ftx.request({
-    //     method: 'POST',
-    //     path: '/orders',
-    //     data: {
-    //         market: 'ETH-PERP',
-    //         size: 0.001,
-    //         side: 'sell',
-    //         type: 'market',
-    //         price: null
-    //     }
-    // }).then(console.log).catch(console.log);
+    if(onlyPositionsWithSize.length > 0) {
+        onlyPositionsWithSize.forEach(position => {
+            ftx.request({
+                method: 'POST',
+                path: '/orders',
+                data: {
+                    market: position.future,
+                    size: position.size,
+                    // buy = sell, sell = buy
+                    side: position.side === 'buy' ? 'sell' : 'buy',
+                    type: 'market',
+                    price: null
+                }
+            }).then(console.log).catch(err => console.log(err));
+        })
+    } else {
+        console.log('No open orders');
+    }
 }
 
-// marketOrder('eth');
+// marketOrder('btc', 'buy', 0.0001);
 
 // calculatePortfolio()
 
-closeOrders()
+// closeOrders()
